99ecb9cd756eeac74da144521354a4560387167a
timur <timurkkq@gmail.com>
2025-11-05 22:24:20 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Deushev_Gvozdeva_lab13;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        /*Button myButton = new Button();
+        myButton.Content = "B";
+
+        Grid.SetRow(myButton, 3);
+        Grid.SetColumn(myButton, 4);
+
+        Grid myGrid = (Grid)FindName("myGrid");
+
+        myGrid.Children.Add(myButton);*/
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        MessageBox.Show("Hello world");
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e) {
+        MessageBox.Show("Hello world");
+    }
+}
